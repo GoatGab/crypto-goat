bc879dddfee4e5d85dd806b87abf85558199ae47
GoatGab <87725365+GoatGab@users.noreply.github.com>
2022-04-28 21:18:48 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# crypto-goat
-A javascript cryptography library
+# CRYPTO-GOAT
+## _Javascript Cryptography library_
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+crypto-goat is a simple, optimized, not super secure, npm cryptography module.
+
+## Features
+
+- Encryption and Decryption
+- Message Encoding
+- ✨Optimizations✨
+
+## Installation
+
+crypto-goat requires [Node.js](https://nodejs.org/) to run.
+
+Install the module and Dependencies to use the library.
+
+```sh
+cd your-directory
+npm i crypto-goat
+```
+
+## Development
+
+How can I use this library??
+
+Importing in the project:
+```sh
+const cryptoGoat = require('crypto-goat')
+```
+
+Keys generation:
+
+```sh
+let keys = generateKeyPair(p, q);
+```
+The function returns an array containing PublicKey and PrivateKey, both will be generated based on the same parameters `P` and `Q`, for P is recommended a small prime number, for the Q is recommended a big prime number with at least 6 digits (it won't work with less than 6 digits)
+
+Encrypting a message:
+
+```sh
+let publicKey = keys[0];
+let encrypted = publicKey.Encrypt('Hiii :)')
+```
+`encrypted` is going to be a `Message` object containing DF (Division Factors) array and a Message array
+
+Decrypting a message:
+
+```sh
+let privateKey = keys[1];
+let decrypted = privateKey.Decrypt(encrypted)
+```
+`decrypted` is going to be a `Message` object containing only a Message array
+
+## How it Works
+crypto-goat is an `RSA` algorithm implementation. Since the algorithm uses an exponentiation operator at a certain point I had to optimize many things to get the module to work fast.
+First of all since after the exponentiation there was a modulo operator I converted the project to use `bigInt` library which contains a function called 'modPow' that optimizes the process.
+Since the algorithm needs integers i had to find a way to encode string messages to integer numbers, by doing that i inevitably stumble across the problem that with big string a big integer would get encoded and the algorithm would be slow, so i decided to do two things:
+- Divide the numbers by the biggest division factor available and store that factor in the message object sent across to be decrypted
+- Separate the text in multiple chunks of variable size and store them as an array with a single division factor for all of them. The message object is then going to be a collection of two smaller arrays: the message array containing the reduced version of the encrypted message and the division factors array that contains the factors to make the message array decryptable, this adds a little computation to add but a great comeback in performance.
+
+The project is open source and if you have some optimizations to do feel free to propose changes to the directory.
+
+## License
+
+MIT
+
+**WARNING: this library is done for educational purposes only and it's still in development, I don't recommend using it in serious projects**
+
---
+++ lib.js
@@ -0,0 +1,251 @@
+const bigInt = require('big-integer')
+
+function gcd(k, n) {
+    return k ? gcd(n % k, k) : n;
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function encode(str) {
+    const codes = str
+        .split('')
+        .map(i => i.charCodeAt())
+        .join('');
+
+    return Number(codes);
+}
+
+function decode(code) {
+    const stringified = code.toString();
+    let string = '';
+
+    for (let i = 0; i < stringified.length; i += 2) {
+        let num = Number(stringified.substr(i, 2));
+
+        if (num <= 30) {
+            string += String.fromCharCode(Number(stringified.substr(i, 3)));
+            i++;
+        } else {
+            string += String.fromCharCode(num);
+        }
+    }
+
+    return string;
+}
+
+exports.PublicKey = class {
+    constructor(p, q) {
+        this.p = p;
+        this.q = q;
+        this.m;
+        this.a;
+        this.initialize();
+    }
+
+    initialize() {
+        this.m = this.p * this.q;
+        this.a = this.generateA();
+    }
+
+    generateCoprimesAmount() {
+        return (this.p - 1) * (this.q - 1);
+    }
+
+    generateA() {
+        for (let i = 2; i < this.generateCoprimesAmount(); i++) {
+            if (gcd(this.m, i) == 1 && gcd(this.generateCoprimesAmount(), i) == 1) {
+                return i;
+            }
+        }
+    }
+
+    Encrypt(message) {
+        message = EncodeMessage(message, 2);
+        let DF = [];
+        for (let i = 0; i < message.length; i++) {
+            DF.push(getGreaterDivisor(message[i]))
+            message[i] /= DF[i];
+        }
+        let closerPrime = getCloserPrime(max(message) + 1);
+        // if (this.q < closerPrime) {
+        //     this.q = closerPrime;
+        //     this.initialize();
+        // }
+        if (message instanceof Array) {
+            for (let i = 0; i < message.length; i++) {
+                message[i] = bigInt(message[i]).modPow(bigInt(this.a), bigInt(this.m));
+            }
+            return new exports.Message(message, DF);
+        } else {
+            return new exports.Message(bigInt(message).modPow(bigInt(this.a), bigInt(this.m)), DF[0]);
+        }
+    }
+}
+
+exports.PrivateKey = class {
+    constructor(p, q, publicKey) {
+        this.p = p;
+        this.q = q;
+        this.m;
+        this.publicKey = publicKey;
+        this.a;
+        this.initialize()
+    }
+
+    initialize() {
+        this.m = this.p * this.q;
+        this.a = this.generateA();
+    }
+
+    generateCoprimesAmount() {
+        return (this.p - 1) * (this.q - 1);
+    }
+
+    generateA() {
+        let phi = this.generateCoprimesAmount();
+        let candidates = [];
+        for (let i = 0; i < this.publicKey.a * phi; i++) {
+            if ((this.publicKey.a * i) % phi == 1) {
+                candidates.push(i);
+                break;
+            }
+        }
+        return candidates[0];
+    }
+
+    Decrypt(message, DF) {
+        // if (this.publicKey.q != this.q) {
+        //     this.q = this.publicKey.q;
+        //     this.initialize();
+        // }
+        if (message instanceof Array) {
+            for (let i = 0; i < message.length; i++) {
+                message[i] = bigInt(message[i]).modPow(bigInt(this.a), bigInt(this.m));
+            }
+            return new exports.Message(DecodeMessage(message, DF).join(''));
+        } else {
+            return new exports.Message(bigInt(message).modPow(bigInt(this.a), bigInt(this.m)));
+        }
+    }
+
+    Decrypt(m) {
+        // if (this.publicKey.q != this.q) {
+        //     this.q = this.publicKey.q;
+        //     this.initialize();
+        // }
+        if (m.message instanceof Array) {
+            for (let i = 0; i < m.message.length; i++) {
+                m.message[i] = bigInt(m.message[i]).modPow(bigInt(this.a), bigInt(this.m));
+            }
+            return new exports.Message(DecodeMessage(m.message, m.DF).join(''));
+        } else {
+            return new exports.Message(bigInt(m.message).modPow(bigInt(this.a), bigInt(this.m)));
+        }
+    }
+}
+
+exports.Message = class {
+    constructor(message, DF = null) {
+        this.message = message;
+        this.DF = DF;
+    }
+}
+
+function Encrypt(key, content) {
+    if (content instanceof Array) {
+        for (let i = 0; i < content.length; i++) {
+            content[i] = bigInt(content[i]).modPow(bigInt(key.a), bigInt(key.m));
+        }
+        return content;
+    } else {
+        return bigInt(content).modPow(bigInt(key.a), bigInt(key.m));
+    }
+}
+
+function max(arr) {
+    let currentMax = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > currentMax) {
+            currentMax = arr[i];
+        }
+    }
+    return currentMax;
+}
+
+function Decrypt(key, content) {
+    if (content instanceof Array) {
+        for (let i = 0; i < content.length; i++) {
+            content[i] = bigInt(content[i]).modPow(bigInt(key.a), bigInt(key.m));
+        }
+        return content;
+    } else {
+        return bigInt(content).modPow(bigInt(key.a), bigInt(key.m));
+    }
+}
+
+function getGreaterDivisor(n) {
+    for (let i = Math.floor(n / 2); i > 0; i--) {
+        if (n % i == 0) {
+            return i;
+        }
+    }
+    return 1;
+}
+
+function isPrime(n) {
+    if (n == 1) {
+        return false;
+    }
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function getCloserPrime(n) {
+    for (let i = n + 1;; i++) {
+        if (isPrime(i)) {
+            return i;
+        }
+    }
+}
+
+function EncodeMessage(m, cs) {
+    let chunkSize = cs;
+    let chunks = chunkString(m, chunkSize);
+    for (let i = 0; i < chunks.length; i++) {
+        chunks[i] = encode(chunks[i]);
+    }
+    return chunks;
+}
+
+function DecodeMessage(m, DF) {
+    let chunks = m;
+    for (let i = 0; i < chunks.length; i++) {
+        chunks[i] = decode(chunks[i] * DF[i]);
+    }
+    return chunks;
+}
+
+function chunkString(str, length) {
+    return str.match(new RegExp('.{1,' + length + '}', 'g'));
+}
+
+function chunkInteger(n, length) {
+    str = n.toString();
+    str = str.match(new RegExp('.{1,' + length + '}', 'g'));
+    for (let i = 0; i < str.length; i++) {
+        str[i] = Number(str[i]);
+    }
+    return str;
+}
+
+exports.generateKeyPair = function(p, q) {
+    let puk = new exports.PublicKey(p, q);
+    let prk = new exports.PrivateKey(p, q, puk);
+    return [puk, prk];
+}
